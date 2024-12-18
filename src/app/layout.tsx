b3699cbe67a5be96a454545f2ac4652143e407e8
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2024-12-17 23:38:11 -0300

    feat: add theme toggle and home wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/theme-provider";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeToggle } from "@/components/theme-toggle";
+import { Menu } from "lucide-react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,11 +27,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} dark:bg-slate-950 bg-slate-50 antialiased min-h-screen `}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <header className="w-full px-10 py-6 flex justify-between items-center mb-10">
+            <h1 className="text-5xl font-bold tracking-widest">
+              CHEAT MEAL TRACKER
+            </h1>
+            <div className="flex items-center gap-20 h-full">
+              <p className="text-xl font-semibold tracking-wider font-mono">
+                Limit: 2/week
+              </p>
+              <ThemeToggle />
+              <Menu size={40} />
+            </div>
+          </header>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,38 @@
+import { Card, CardContent } from "@/components/ui/card";
+
 export default function Home() {
-  return <h1>Cheat meal tracker</h1>;
+  return (
+    <main className="flex flex-col items-start justify-center w-11/12 mx-auto h-full">
+      <section className="flex items-center justify-between w-full">
+        <Card className="w-auto border-2 border-green-600 rounded-xl h-full">
+          <CardContent className="flex flex-col items-start justify-center gap-2 py-8 px-8">
+            <span className="text-5xl font-medium">Week xx of 20xx</span>
+            <p className="text-4xl font-medium flex items-center gap-2 font-mono">
+              <span className="text-6xl text-green-600 ">0</span> Cheat meals
+              this week
+            </p>
+          </CardContent>
+        </Card>
+        <Card className="w-auto border-2 border-green-600 rounded-xl h-full">
+          <CardContent className="flex flex-col items-start justify-center gap-2 py-8 px-8">
+            <span className="text-5xl font-medium">xx/yy/zzzz</span>
+            <p className="text-4xl font-medium flex items-center gap-2 font-mono">
+              <span className="text-6xl text-green-600 ">0</span> Cheat meals
+              today
+            </p>
+          </CardContent>
+        </Card>
+        <Card className="w-auto border-2 border-green-600 rounded-xl">
+          <CardContent className="flex flex-col items-center justify-center gap-2 py-8 px-8">
+            <p className="text-4xl font-medium flex items-center gap-2 font-mono ">
+              <span className="text-8xl text-green-600 ">0</span>
+            </p>
+            <span className="text-3xl font-medium">
+              Days since last cheat meal
+            </span>
+          </CardContent>
+        </Card>
+      </section>
+    </main>
+  );
 }
