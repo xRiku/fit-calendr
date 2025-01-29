7dab8f5b5a4f1c52bc33fe092ba4582202148fd3
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2025-01-29 08:52:08 -0300

    fix: change env variable

--- src/env.ts
+++ src/env.ts
@@ -9,9 +9,9 @@ export const env = createEnv({
     GOOGLE_CLIENT_SECRET: z.string().min(1),
   },
   client: {
-    NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
+    NEXT_PUBLIC_VERCEL_URL: z.string().min(1),
   },
   experimental__runtimeEnv: {
-    NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
   },
 });
