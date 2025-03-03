1338a327383d83bb659b45b20ab30a4dc64abf4f
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2025-03-03 17:07:42 -0300

    feat: implement session-based redirection in middleware for private and login pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,38 +1,25 @@
-// // import { auth } from "@/auth";
+import { type NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth";
 
-// // export default auth;
+export async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const sessionCookie = getSessionCookie(request);
 
-// import { NextRequest, NextResponse } from "next/server";
-// import { getSessionCookie } from "better-auth";
+  const isOnPrivatePages = pathname.includes("/app");
+  const isOnLoginPage = pathname.includes("/auth");
 
-// export async function middleware(request: NextRequest) {
-//   // const sessionCookie = getSessionCookie(request);
-//   // if (!sessionCookie) {
-//   //   return NextResponse.redirect(new URL("/", request.url));
-//   // }
-//   return NextResponse.next();
-// }
+  if (!sessionCookie && isOnPrivatePages) {
+    return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+  }
 
-// export const config = {
-//   matcher: [
-//     "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
-//   ],
-// };
-
-import { NextResponse } from "next/server";
-// import { type NextRequest, NextResponse } from "next/server";
-
-export async function middleware() {
-  // export async function middleware(request: NextRequest) {
-  // const sessionCookie = getSessionCookie(request); // Optionally pass config as the second argument if cookie name or prefix is customized.
-  // if (!sessionCookie) {
-  //   return NextResponse.redirect(new URL("/", request.url));
-  // }
+  if (sessionCookie && isOnLoginPage) {
+    return NextResponse.redirect(new URL("/app/dashboard", request.url));
+  }
   return NextResponse.next();
 }
 
 export const config = {
   matcher: [
     "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
-  ], // Specify the routes the middleware applies to
+  ],
 };
