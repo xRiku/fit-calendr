e01c5dfbaa6e861dfd10d53c127d1f240d154e52
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2026-01-28 16:01:19 -0300

    refactor: reorganize imports and improve component structure in dialog components
    - Updated import statements in day-info-dialog and dialog files for consistency.
    - Enhanced formatting and readability of the dialog components.
    - Added DialogDescription to the dialog component for better accessibility and information display.

--- src/components/day-info-dialog.tsx
+++ src/components/day-info-dialog.tsx
@@ -1,35 +1,40 @@
 "use client";
 
 import { useModalStore } from "@/stores/day-info-modal";
-import { DialogDescription } from "@radix-ui/react-dialog";
 import DayInfoForm from "./day-info-form";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "./ui/dialog";
 
 export default function DayInfoDialog() {
-	const {
-		dayInfoModalState,
-		toggleDayInfoModalState,
-		dayInfoType,
-		selectedDayInfo,
-	} = useModalStore();
+  const {
+    dayInfoModalState,
+    toggleDayInfoModalState,
+    dayInfoType,
+    selectedDayInfo,
+  } = useModalStore();
 
-	return (
-		<Dialog
-			open={dayInfoModalState}
-			onOpenChange={() => toggleDayInfoModalState()}
-		>
-			<DialogContent className="sm:max-w-[425px]">
-				<DialogHeader aria-describedby="Day Info Form">
-					<DialogTitle className="font-extrabold text-2xl">
-						{dayInfoType === "create" && "Add info for the day"}
-						{dayInfoType === "edit" && "Edit info for the day"}
-					</DialogTitle>
-					<DialogDescription>
-						{selectedDayInfo?.date.toDateString()}
-					</DialogDescription>
-				</DialogHeader>
-				<DayInfoForm />
-			</DialogContent>
-		</Dialog>
-	);
+  return (
+    <Dialog
+      open={dayInfoModalState}
+      onOpenChange={() => toggleDayInfoModalState()}
+    >
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader aria-describedby="Day Info Form">
+          <DialogTitle className="font-extrabold text-2xl">
+            {dayInfoType === "create" && "Add info for the day"}
+            {dayInfoType === "edit" && "Edit info for the day"}
+          </DialogTitle>
+          <DialogDescription>
+            {selectedDayInfo?.date.toDateString()}
+          </DialogDescription>
+        </DialogHeader>
+        <DayInfoForm />
+      </DialogContent>
+    </Dialog>
+  );
 }
